f5a81d8d8e40d6865a7375416f4925a13ad4eda8
knhoooo <knh000125@hanyang.ac.kr>
2025-02-21 15:04:16 +0900

    콘솔 로딩바

---
+++ Loading/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(2000);//1000 == 1초(1000밀리초)
+            Console.Clear();//콘솔 화면 지우기
+            Console.WriteLine("■■■■■■■■■■");
+            Console.Clear();
+            Console.WriteLine("텍스트 엘든링");
+
+            Console.WriteLine("스토리1");
+        }
+    }
+}
