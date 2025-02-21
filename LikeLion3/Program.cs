eb5b3dbbf2c085edb6b13068de941914e03f5e14
knhoooo <knh000125@hanyang.ac.kr>
2025-02-21 10:42:59 +0900

    리터럴

---
+++ LikeLion3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //리터럴은 변하지 않는 값으로 변수에 할당하거나 직접 사용할 수 있다.
+            int hp = 100;//정수형 리터럴
+            double att = 56.7;//실수형 리터럴
+            char grade = 'A';//문자 리터럴
+            string name = "Alice";//문자열 리터럴
+
+            Console.WriteLine(hp); //출력 : 100
+            Console.WriteLine(att); //출력 : 56.7
+            Console.WriteLine(grade); //출력 : A
+            Console.WriteLine(name); //출력 : Alice
+        }
+    }
+}
