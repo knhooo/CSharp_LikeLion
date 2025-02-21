0aed016542dd3e36ec119bb92b75bc2243234514
knhoooo <knh000125@hanyang.ac.kr>
2025-02-21 10:23:11 +0900

    변수

---
+++ LikeLion2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //변수 선언
+            int age;//정수형 변수
+            age = 25;//변수에 값 저장
+
+            Console.WriteLine(age);//WriteLine : 줄바꿈
+            Console.Write(age);//변수에 저장된 값을 출력  25
+        }
+    }
+}
